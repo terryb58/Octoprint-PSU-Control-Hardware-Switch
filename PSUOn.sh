eb268138275147098b823f73db2ca371597c4072
terryb58 <terry.bogayong@gmail.com>
2019-06-19 12:00:40 -0400

    Add files via upload

--- PSUOn.sh
+++ PSUOn.sh
@@ -3,5 +3,7 @@
 # You'll need to substitute your own values for:
 #   - API key from your instance of Octoprint
 #   - Username and password for logging into OctoPrint
+#
+# Questions? Email me at terry.bogayong@mak3rhq.command
 curl -s -H "Content-Type: application/json" -H "X-Api-Key: __YOUR_API_KEY_FROM_OCTOPRINT__" -X POST -d '{ "command":"turnPSUOn" }' -u username:password http://localhost/api/plugin/psucontrol
 
